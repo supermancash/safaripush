fab7697375462d4b3f78710fd7b4fe2eaed588f0
supermancash <alexanderio.emilio@gmail.com>
2021-10-13 17:36:42 +0200

    double quotes instead of single for if

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,25 @@ function App() {
         } else {
             document.getElementById("mainDiv").innerHTML = "Please use safari to access this app"
         }
-    })
+    });
+
+    function checkRemotePermission(permissionData, onLoad) {
+        if (permissionData.permission === 'default' && !onLoad) {
+            window.safari.pushNotification.requestPermission(
+                'https://safaripushapi.herokuapp.com',
+                'web.com.safaripushapi',
+                {},
+                checkRemotePermission
+            ).catch((err) => console.log(err));
+        } else if (permissionData.permission === 'denied') {
+            setPermission("denied");
+            document.getElementById("subscribeParagraph").innerHTML = "Please press accept on the notification prompt. ";
+        } else if (permissionData.permission === 'granted') {
+            setPermission("granted");
+            document.getElementById("subscribeButtonDiv").innerHTML = "";
+            console.log(permissionData);
+        }
+    }
 
     let permissionData;
 
@@ -51,31 +69,13 @@ function App() {
         })
     }
 
-    function checkRemotePermission(permissionData, onLoad) {
-        if (permissionData.permission === 'default' && !onLoad) {
-            window.safari.pushNotification.requestPermission(
-                'https://safaripushapi.herokuapp.com',
-                'web.com.safaripushapi',
-                {},
-                checkRemotePermission
-            ).catch((err) => console.log(err));
-        } else if (permissionData.permission === 'denied') {
-            setPermission("denied");
-            document.getElementById("subscribeParagraph").innerHTML = "Please press accept on the notification prompt. ";
-        } else if (permissionData.permission === 'granted') {
-            setPermission("granted");
-            document.getElementById("subscribeButtonDiv").innerHTML = "";
-            console.log(permissionData);
-        }
-    }
-
     return (
         <div id="mainDiv" className="App">
             <div id="subscribeButtonDiv">
                 <button className="subscribeButton" onClick={subscribeHandler}>Subscribe</button>
                 <p id="subscribeParagraph"/>
             </div>
-            {permission==='granted' ? <NotificationForm userToken={permissionData.deviceToken}/> : <div/>}
+            {permission==="granted" ? <NotificationForm userToken={permissionData.deviceToken}/> : <div/>}
         </div>
     );
 }
